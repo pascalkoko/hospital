ccebbbb84f2acd8d8e39ae36952f6bc13dee0a3a
pascal_koko <p.koko@nuru.cd>
2023-12-06 11:22:20 +0200

    validation du format de la date dans la Base des données avec l'annotation @Tempory de Spring Jakarta Validation

--- src/main/java/com/pixel/hospital/entities/Patient.java
+++ src/main/java/com/pixel/hospital/entities/Patient.java
@@ -1,9 +1,6 @@
 package com.pixel.hospital.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
@@ -23,13 +20,12 @@ public class Patient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @NotEmpty
     @Size(min = 4, max=20)
     private String nomPatient;
+    @Temporal(TemporalType.DATE)
     private Date dateNaissance;
     private boolean malade;
-
     @Min(20)
     private int score;
 }
